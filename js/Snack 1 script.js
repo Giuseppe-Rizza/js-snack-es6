dda973d3923ef0abc493c9de35e3f6061ef54bc6
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-01-16 18:38:33 +0100

    [feat] Add array

---
+++ js/Snack 1 script.js
@@ -0,0 +1,32 @@
+//Creo array oggetti bici
+
+const cycles = [
+
+    {
+        "name" : "Bianchi",
+        "weight" : 4
+    },
+
+    {
+        "name" : "Cannondale",
+        "weight" : 2
+    },
+
+    {
+        "name" : "Colnago",
+        "weight" : 1
+    },
+
+    {
+        "name" : "Scott",
+        "weight" : 5
+    },
+
+    {
+        "name" : "Trek",
+        "weight" : 3
+    }
+
+];
+
+console.log(cycles);
